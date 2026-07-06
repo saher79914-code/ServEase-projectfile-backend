96b5164a9ae3544f0508cba81cbf1e9d09101b2f
unknown <ayesha079881@gmail.com>
2026-07-06 16:55:17 +0500

    ter

--- config/db.js
+++ config/db.js
@@ -1,22 +1,24 @@
-// config/db.js
-const mysql = require("mysql2/promise"); 
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "servease",
+  port: 3310,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
 
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
-  database: "auth_db",
-  waitForConnections: true,
-  connectionLimit: 10,
+  database: "healthy_wealthy_db",
 });
 
-db.getConnection()
-  .then((conn) => {
-    console.log("✅ DB Connected");
-    conn.release();
-  })
-  .catch((err) => {
-    console.error("❌ DB Connection Failed:", err.message);
-  });
-
 module.exports = db;
+
